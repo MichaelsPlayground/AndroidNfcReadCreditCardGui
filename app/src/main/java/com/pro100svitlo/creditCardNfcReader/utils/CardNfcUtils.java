daa362e14c9a0786caed0f6fd502a542f54c4983
michaelfehr <michael_fehrgmx.de>
2022-05-13 20:54:45 +0200

    updated

--- app/src/main/java/com/pro100svitlo/creditCardNfcReader/utils/CardNfcUtils.java
+++ app/src/main/java/com/pro100svitlo/creditCardNfcReader/utils/CardNfcUtils.java
@@ -8,6 +8,7 @@ import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.IsoDep;
 import android.nfc.tech.NfcA;
+import android.os.Build;
 
 /**
  * Created by pro100svitlo on 31.03.16.
@@ -25,9 +26,20 @@ public class CardNfcUtils {
 
     public CardNfcUtils(final Activity pActivity) {
         mActivity = pActivity;
+        Context mContext = mActivity.getBaseContext();
         mNfcAdapter = NfcAdapter.getDefaultAdapter(mActivity);
-        mPendingIntent = PendingIntent.getActivity(mActivity, 0,
-                new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        // taken from: https://stackoverflow.com/a/70227216/8166854
+        Intent workingIntent = new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            mPendingIntent = PendingIntent.getActivity(mContext,
+                    0, workingIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+
+        }else {
+            //mPendingIntent = PendingIntent.getActivity(mContext, 0, workingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            mPendingIntent = PendingIntent.getActivity(mActivity, 0, new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        }
+        // Android 12 / SDK 31 will give an error, use above method
+        // mPendingIntent = PendingIntent.getActivity(mActivity, 0, new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
     }
 
     public void disableDispatch() {
